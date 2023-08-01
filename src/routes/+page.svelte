ad3594e1c0d430a3a605726a5d10df49cafd1be1
Landay <67254560+landiehan@users.noreply.github.com>
2023-08-01 17:50:08 +0800

    fix: decrease card and img size on large screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@
 </script>
 
 <div class="relative z-0 h-screen flex flex-col items-center justify-center bg-pink-200">
-	<div class="bg-white/80 w-1/2 shadow-sm rounded-lg backdrop-blur-3xl px-3 pt-3 pb-4">
+	<div class="bg-white/80 w-1/2 max-w-xs shadow-sm rounded-lg backdrop-blur-3xl px-3 pt-3 pb-4">
 		<div class="flex flex-col items-center justify-center">
 			<h1 class="text-3xl">👨‍💻</h1>
 			<span class="text-xs text-zinc-600 my-3">Frontend Developer</span>
@@ -37,7 +37,7 @@
 		<img
 			src={tool.iconSrc}
 			alt=""
-			class="object-cover blur-lg absolute -z-10 w-3/4 h-auto"
+			class="object-contain blur-lg absolute -z-10 w-3/4 h-auto max-w-2xl"
 			transition:fade
 		/>
 	{/key}
